6f568d0ef00cf7b82335d569a1ae200358dc40c1
JustinM1 <JustinM1@users.noreply.github.com>
2017-05-20 08:30:40 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "VaporValidation",
     dependencies: [
         .Package(url: "https://github.com/vapor/validation.git", majorVersion: 1),
-        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2,
+        .Package(url: "https://github.com/vapor/vapor.git", majorVersion: 2),
     ]
 )
